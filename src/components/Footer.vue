95d7be6f6b0b1c96a0fe6c60be6f582ae108e65c
NoSineB <132141781+NoSineB@users.noreply.github.com>
2024-05-14 19:48:04 +0530

    Dist Updated

--- src/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="mt-8 py-4 bg-[#031d44] text-[#fffff2] w-full flex justify-center items-center p-2">
-        Made with ❤️ By	&nbsp;<a href="https://www.ieeesbcev.org/" class="hover:text-[#F87666]">IEEE SB CEV WEB TEAM</a>
-    </div>
-</template>
